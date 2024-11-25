045505a8f39cab171c520aceaa6c07e711c92d0e
Sergio Galera <sgaleraalq@gmail.com>
2024-11-25 09:24:14 +0100

    Filtering undetermined reads to copy final folder

--- command.sh
+++ command.sh
@@ -1,7 +1,7 @@
 nextflow run main.nf \
     -profile singularity \
     --input /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/sample_sheet_for_runs.csv \
-    --outdir /data/scratch/LAB/temp_demultiplex/20241113_TAAV \
+    --outdir /data/scratch/LAB/temp_demultiplex/20241113_TAAV_UMIs \
     -resume
 
 
