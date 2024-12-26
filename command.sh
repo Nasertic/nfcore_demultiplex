ca4db0009f049859c0e03f0f78b1f41a37b6868e
Sergio Galera <sgaleraalq@gmail.com>
2024-12-26 14:53:08 +0100

    Just testing https connection

--- command.sh
+++ command.sh
@@ -1,13 +1,13 @@
-nextflow run main.nf \
-    -profile singularity \
-    --input /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/sample_sheet_for_runs.csv \
-    --outdir /data/scratch/LAB/temp_demultiplex/20241219_example \
-    -resume
+# nextflow run main.nf \
+#     -profile singularity \
+#     --input /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/sample_sheet_for_runs.csv \
+#     --outdir /data/scratch/LAB/temp_demultiplex/20241219_example \
+#     -resume
 
 
 # # TESTING
-# nextflow run main.nf \
-#     -profile singularity \
-#     --input /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/sample_sheet_for_testing.csv \
-#     --outdir /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/example \
-#     -resume
+nextflow run main.nf \
+    -profile singularity \
+    --input /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/sample_sheet_for_testing.csv \
+    --outdir /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/example \
+    -resume
