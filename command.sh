b018b0956e07d47f5563d8035e7667039d2bf513
Sergio Galera <sgaleraalq@gmail.com>
2024-12-30 11:44:17 +0100

    Some files deleted

--- command.sh
+++ command.sh
@@ -1,13 +1,13 @@
-# nextflow run main.nf \
-#     -profile singularity \
-#     --input /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/sample_sheet_for_runs.csv \
-#     --outdir /data/scratch/LAB/temp_demultiplex/20241219_example \
-#     -resume
-
-
-# # TESTING
 nextflow run main.nf \
     -profile singularity \
-    --input /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/sample_sheet_for_testing.csv \
-    --outdir /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/test_new_dragen \
+    --input /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/sample_sheet_for_runs.csv \
+    --outdir /data/scratch/LAB/temp_demultiplex/20241219_example \
     -resume
+
+
+# # # TESTING
+# nextflow run main.nf \
+#     -profile singularity \
+#     --input /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/sample_sheet_for_testing.csv \
+#     --outdir /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/test_new_dragen \
+#     -resume
