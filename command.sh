1e83efbb5de09486be4bbca576d2274c0f43bfb3
Sergio Galera <sgaleraalq@gmail.com>
2025-03-21 14:39:24 +0100

    Dragen v4 configurations changed

--- command.sh
+++ command.sh
@@ -1,8 +1,15 @@
-nextflow run main.nf \
+nextflow run /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/main.nf \
     -profile singularity \
     --input /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/sample_sheet_for_runs.csv \
-    --outdir /data/scratch/LAB/temp_demultiplex/20241219_Syntax_3 \
+    --outdir /data/scratch/LAB/temp_demultiplex/20241219_TAAV_2 \
     -resume
+
+# # SYNTAX
+# nextflow run /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/main.nf \
+#     -profile singularity \
+#     --input /data/scratch/LAB/temp_demultiplex/nfcore_demultiplex/sgalera/nfcore_demultiplex/sample_sheet_for_syntax.csv \
+#     --outdir /data/scratch/LAB/temp_demultiplex/20241219_Syntax_example \
+#     -resume
 
 
 # # # TESTING
